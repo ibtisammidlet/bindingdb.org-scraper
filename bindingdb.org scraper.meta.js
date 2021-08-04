c60a45f7d963f6d4b76250bad797b7a9fc86dae2
ibtisam midlet <storebitid@gmail.com>
2021-08-04 17:34:57 +0100

    Update bindingdb.org scraper.meta.js

--- bindingdb.org scraper.meta.js
+++ bindingdb.org scraper.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        youtube watched videos hider
-// @version     0.1
+// @version     0.2
 // @updateURL   https://github.com/ibtisammidlet/bindingdb.org-scraper/raw/main/bindingdb.org%20scraper.meta.js
 // @downloadURL https://github.com/ibtisammidlet/bindingdb.org-scraper/raw/main/bindingdb.org%20scraper.user.js
 // ==/UserScript==
