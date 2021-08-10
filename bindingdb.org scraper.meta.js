64d0c7899b370799766a3841c79c6097df1829a5
ibtisam midlet <storebitid@gmail.com>
2021-08-10 03:44:56 +0100

    Update bindingdb.org scraper.meta.js

--- bindingdb.org scraper.meta.js
+++ bindingdb.org scraper.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        bindingdb.org scraper
-// @version     0.3
+// @version     0.4
 // @updateURL   https://github.com/ibtisammidlet/bindingdb.org-scraper/raw/main/bindingdb.org%20scraper.meta.js
 // @downloadURL https://github.com/ibtisammidlet/bindingdb.org-scraper/raw/main/bindingdb.org%20scraper.user.js
 // ==/UserScript==
